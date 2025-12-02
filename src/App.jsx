f0a465402d20040f2d98c4bf6054561f67578c09
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-02 18:19:27 -0300

    Extracted first word from data

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import { defaultExternalConditions } from "vite";
 
-CAT_END_POINT_IMAGE = `https://cataas.com/cat/says/${'lorem'}?fontSize=50&fontColor=red&json=true`
+const CAT_END_POINT_FACT = "https://catfact.ninja/fact";
+const CAT_END_POINT_IMAGE = `https://cataas.com/cat/says/${"first word"}?fontSize=50&fontColor=red&json=true`;
 
 function App() {
-  const [fact, setFact] = useState('lorem ipso cat fat')
-  const [image, setImage] = useState('')
+  const [fact, setFact] = useState("lorem ipso cat fat");
+  const [image, setImage] = useState(null);
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
-    .then(res => res.json())
-    .then((data) => {
-      setFact(data.fact)
-    })
+    fetch(CAT_END_POINT_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data
+        setFact(fact);
+        const firstWord = fact.split(' ')[0]
+        console.log(firstWord);
+        
+      });
 
     fetch(CAT_END_POINT_IMAGE)
-    .then(res => res.json())
-    .then((data) => {
-      setImage(data.fact)
-    })
-
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.fact);
+      });
+  }, []);
 
   return (
     <>
       <h1>App de gatitos </h1>
-      <p>{fact}</p>
-      <img src={image} alt="imagen gato naranja"/>
+      {fact && <p>{ fact }</p>}
+      <img src={image} alt="imagen gato naranja" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
