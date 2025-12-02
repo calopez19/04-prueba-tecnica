3d8cd3e9b7c5995fe7a8c0cc1a1ce8de4f5ccc28
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-02 17:45:12 -0300

    fetching fact and images from source

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
+CAT_END_POINT_IMAGE = `https://cataas.com/cat/says/${'lorem'}?fontSize=50&fontColor=red&json=true`
+
 function App() {
+  const [fact, setFact] = useState('lorem ipso cat fat')
+  const [image, setImage] = useState('')
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+    .then(res => res.json())
+    .then((data) => {
+      setFact(data.fact)
+    })
+
+    fetch(CAT_END_POINT_IMAGE)
+    .then(res => res.json())
+    .then((data) => {
+      setImage(data.fact)
+    })
+
+  },[])
 
   return (
     <>
-      <h1>Prueba tecnica</h1>
+      <h1>App de gatitos </h1>
+      <p>{fact}</p>
+      <img src={image} alt="imagen gato naranja"/>
     </>
   )
 }
