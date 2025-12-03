884e951e3703da8d9359333b448138909728aff8
camiloUbuntu <53837383+calopez19@users.noreply.github.com>
2025-12-03 18:09:04 -0300

    refactor: update fact fetching logic and improve image URL construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,71 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
-import { defaultExternalConditions } from "vite";
 
+const CAT_END_POINT_IMAGE_Part1 = "https://cataas.com/cat/says/";
+const CAT_END_POINT_IMAGE_Part2 = "?fontSize=50&fontColor=red&json=true";
 const CAT_END_POINT_FACT = "https://catfact.ninja/fact";
-const CAT_END_POINT_IMAGE = `https://cataas.com/cat/says/${"first word"}?fontSize=50&fontColor=red&json=true`;
 
 function App() {
-  const [fact, setFact] = useState("lorem ipso cat fat");
+  const [fact, setFact] = useState("");
   const [image, setImage] = useState(null);
-  useEffect(() => {
+
+  const changeFact = () => {
     fetch(CAT_END_POINT_FACT)
       .then((res) => res.json())
       .then((data) => {
-        const { fact } = data
-        setFact(fact);
-        const firstWord = fact.split(' ')[0]
-        console.log(firstWord);
-        
+        const newFact = data.fact;
+        setFact(newFact);
+        const firstThreeWords = newFact.split(" ").slice(0, 3).join(" ");
+        console.log(firstThreeWords);
       });
+  };
 
-    fetch(CAT_END_POINT_IMAGE)
-      .then((res) => res.json())
-      .then((data) => {
-        setImage(data.fact);
-      });
+
+  const handleClick = () => {
+    changeFact();
+  };
+
+
+  useEffect(() => {
+    changeFact();
   }, []);
+
+  useEffect(() => {
+    if (!fact) return;
+    const firstThreeWords = fact.split(" ").slice(0, 3).join(" ");
+    fetch(
+      `${CAT_END_POINT_IMAGE_Part1}${firstThreeWords}${CAT_END_POINT_IMAGE_Part2}`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        const { url } = response;
+        setImage(`${url}`);
+      });
+  }, [fact]);
 
   return (
     <>
       <h1>App de gatitos </h1>
-      {fact && <p>{ fact }</p>}
-      <img src={image} alt="imagen gato naranja" />
+      <button onClick={handleClick}>change fact</button>
+      <section
+        style={{
+          paddingTop: "10px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <p>{fact}</p>
+        {image && (
+          <img
+            src={image}
+            alt="imagen gato naranja"
+            style={{ height: "400px", width: "auto" }}
+          />
+        )}
+      </section>
     </>
   );
 }
